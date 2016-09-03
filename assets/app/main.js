44131e795a0e1b06e6b3a2fcd9511778f24b4292
xmaestro <alexeyfedorov101@gmail.com>
2016-09-04 00:53:32 +0500

    UPdated to Angular 2 RC6

--- assets/app/main.js
+++ assets/app/main.js
@@ -1,5 +1,5 @@
 "use strict";
 const platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-const app_component_1 = require('./app.component');
-platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent);
+const app_module_1 = require('./app.module');
+platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
 //# sourceMappingURL=main.js.map
--- assets/app/main.ts
+++ assets/app/main.ts
@@ -1,3 +1,4 @@
-import {bootstrap}    from '@angular/platform-browser-dynamic';
-import {AppComponent} from './app.component';
-bootstrap(AppComponent);
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule }              from './app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
